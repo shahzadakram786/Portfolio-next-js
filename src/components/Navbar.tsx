6c5abae3d29f9e259b3f0ab4261dfb70d7a0b319
shahzadakram786 <alyakram786@gmail.com>
2024-10-11 17:42:21 +0500

    working on the navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+const navLinks = [
+    {title:"About", path:"#about"},
+    {title:"Portfolio",path:"#about"}
+]
+
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
