0ddfe4e07982c0f7a60f265e9b607207a6a0f436
shahzadakram786 <alyakram786@gmail.com>
2024-10-12 01:13:50 +0500

    Hero section has been done almost

---
+++ src/components/Hero.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import html from '../assets/Html Coding (1).png'
+import code from '../assets/Programming Language (1).png'
+import picture from '../assets/smiling-anime-chibi-cartoon-boy-working-computer-white-background-smiling-anime-chibi-cartoon-boy-working-computer-321914300.png'
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+
+
+const Hero = () => {
+  return (
+
+    <>
+    <div className="relative py-24 overflow-clip bg-gradient-to-t from-emerald-800 to-gray-1000">
+      
+      <div className='absolute rounded-[50%] h-[1300px] w-[3000px] bg-black top-[550px] left-[50%] -translate-x-1/2 bg-[radial-gradient(closest-side,#000_80%,#2B1942)]'>
+      </div>
+      
+       <div className='relative'>
+        <div className="text-8xl font-bold text-center">
+            <h1 className="text-[#98B4CE]">Hi, I am</h1>
+            <h1 className="text-[#E48A57]">Shahzad Akram</h1>
+        </div>
+
+
+      <motion.div
+      className="absolute left-[280px] top-[90px]"
+      drag>
+            <Image
+            src={html}
+            height="190"
+            width="190"
+            alt='computer'
+            draggable="false"
+            >
+
+            </Image>
+
+      </motion.div>
+
+
+      <motion.div
+      className="absolute right-[220px] top-[90px]"
+      drag>
+            <Image
+            src={code}
+            height="200"
+            width="200"
+            alt='computer'
+            draggable="false"
+            >
+
+            </Image>
+
+      </motion.div>
+
+    
+
+    <p className='text-center text-xl max-w-[500px] mx-auto mt-8 text-white/80' >
+        I am a Full-Stack Developer focused on creating websites that provide the vest user experience
+    </p>
+
+    
+    <Image
+    src={picture}
+    alt="profile picture"
+    className='h-auto w-auto mx-auto'>
+
+    </Image>
+
+    </div> 
+
+
+    
+</div>
+    </>
+  )
+}
+
+export default Hero
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,112 @@
-"use client"
+"use client";
+
+import { motion } from "framer-motion";
+import { li } from "framer-motion/client";
+import Link from "next/link";
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const navLinks = [
-    {title:"About", path:"#about"},
-    {title:"Portfolio",path:"#about"}
-]
-
-
+  { title: "About", path: "#about" },
+  { title: "Portfolio", path: "#about" },
+];
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  // 1st nav step
+  // usehook
+  const [nav, setNav] = useState(false);
 
-export default Navbar
+  const toggleNav = () => {
+    setNav(!nav);
+  };
+
+  const closeNav = () => {
+    setNav(false);
+  };
+
+
+  const menuVariants = {
+    open:{
+        x: 0 , 
+        transition:{
+            stiffness:20,
+            damping:15
+
+        }
+
+    },
+    closed:{
+            x: '-100%',
+            trasition:{
+                stiffness:20,
+                damping:15
+                }
+    }
+  }
+
+
+
+
+
+
+  return (
+    <div className="text-white/70 pt-6">
+
+      <div className="hidden md:flex items-center px-4 py-2 mx-auto max-w-[400px]">
+
+        <ul className="flex flex-row p-4 space-x-8" >
+            {navLinks.map((links, index) => (
+            <li key={index}>
+                <Link href={links.path}>
+                <p>{links.title}</p>
+                </Link>
+
+            </li>
+            ))}
+
+            <li>
+                <a href="#contact" className="group">
+                <h1>Contact me</h1>
+                <div className="relative">
+                        <div className="absolute w-2/3 h-1 bg-orange-400 transition-all duration-300 ease-out rounded-full group-hover:w-full"></div>
+
+                        <div className="mt-1 absolute w-1/3 h-1 bg-orange-600 transition-all duration-300 ease-out rounded-full group-hover:w-full"></div>
+                </div>
+                </a>
+            </li>
+        </ul>
+
+      </div>
+
+            {/* for mob version */}
+            <div onClick={toggleNav} className="md:hidden absolute top-5 right-5 border rounded text-white/70 border-white p-2 z-50">
+                {nav ? <AiOutlineClose size={30}/> : <AiOutlineMenu size={30}/>}
+            </div>
+            
+            <motion.div
+            initial={false}
+            animate={nav ? 'open' : 'closed'}
+            variants={menuVariants}
+            className="fixed left-0 top-0 w-full z-40 bg-black/90">
+
+                    <ul className="text-4xl font-semibold my-24 text-center space-y-8 " >
+                        {navLinks.map((link , index) => (
+                            <li key={index}>
+                                <Link href={link.path} onClick={closeNav}>
+                                {link.title}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+            </motion.div>
+
+
+
+    </div>
+
+
+
+  );
+};
+
+export default Navbar;
