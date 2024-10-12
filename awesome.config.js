aaa7cfe3811491185ef0d1398bc161041dd9f288
shahzadakram786 <alyakram786@gmail.com>
2024-10-12 20:14:53 +0500

    push

---
+++ awesome.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://12123412.xyz",
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-    siteUrl: 'https://12123412.xyz',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-  }
+const nextConfig = {};
+
+export default nextConfig;
