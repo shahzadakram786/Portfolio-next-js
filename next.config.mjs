ae2d23f2039d2d0621d3ee3e87e1f5e91055d8d1
shahzadakram786 <alyakram786@gmail.com>
2024-10-12 20:09:47 +0500

    s

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+    siteUrl: 'https://12123412.xyz',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
