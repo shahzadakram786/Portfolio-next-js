8e7e242d9e34110ad09bf04c45f6f5e2742485f4
shahzadakram786 <alyakram786@gmail.com>
2024-10-15 22:43:23 +0500

    Skills page completed

---
+++ src/components/Contact.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import React from 'react'
+import { FaPhone } from 'react-icons/fa'
+
+
+
+
+const Contact = () => {
+  return (
+    <div>
+        
+        <div>
+            <ul>
+              <li>
+                <FaPhone size={140}/>
+              </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
